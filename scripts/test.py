7814e45c18aedd94aec0678e3797ed45789646c5
Fraser Davidson <fraser@gofrontier.com>
2023-12-22 18:09:12 +0000

    WIP

--- scripts/test.py
+++ scripts/test.py
@@ -3,6 +3,6 @@ from helpers.test_terraform import test_terraform
 
 
 if __name__ == "__main__":
-    for dir in [f.path for f in os.scandir(os.path.join(os.getcwd(), "modules")) if f.is_dir()]:  # noqa: E501
+    for dir in [f.path for f in os.scandir(os.path.join(os.getcwd(), "modules")) if f.is_dir()]:
         test_terraform(os.path.join(dir, "src"), False)
         test_terraform(os.path.join(dir, "test"))
