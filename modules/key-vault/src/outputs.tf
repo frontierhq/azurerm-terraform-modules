007fdf4ddd60ad74302c89293a43fb355f8738fa
Fraser Davidson <fraser@gofrontier.com>
2023-12-27 20:38:50 +0000

    WIP

--- modules/key-vault/src/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "id_id" {
-  value = azurerm_key_vault.main.id
-}
-
-output "name" {
-  value = azurerm_key_vault.main.name
-}
--- modules/resource-group/src/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "main" {
-  name     = "rg-${var.zone}-${var.environment}-${lookup(local.short_locations, var.location)}-${local.identifier}"
-  location = var.location
-
-  tags = merge(var.tags, local.tags)
-}
