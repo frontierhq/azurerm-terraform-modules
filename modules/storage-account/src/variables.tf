1c024eee3aa9cec10f5cd2d64ced4413bc7b2e9a
Fraser Davidson <fraser@gofrontier.com>
2024-01-03 19:05:31 +0000

    Add Storage Account module (#11)

---
+++ modules/storage-account/src/variables.tf
@@ -0,0 +1,38 @@
+variable "account_tier" {
+  type    = string
+  default = "Standard"
+}
+
+variable "account_replication_type" {
+  type    = string
+  default = "ZRS"
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "identifier" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "log_analytics_workspace_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "zone" {
+  type = string
+}
