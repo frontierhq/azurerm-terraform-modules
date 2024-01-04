5d11f444ede00e14f4c9aece9ef8d6e1ee949af6
Craig Anderson <1877372+CDA0@users.noreply.github.com>
2024-01-04 14:06:52 +0000

    Add Data Factory module (#21)

---
+++ modules/data-factory/src/outputs.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = azurerm_data_factory.main.id
+}
+
+output "name" {
+  value = azurerm_data_factory.main.name
+}
+
+output "identity" {
+  value = azurerm_data_factory.main.identity
+}
---
+++ modules/data-factory/test/main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  features {}
+}
+
+module "data_factory" {
+  source = "../src"
+
+  environment                = "baz"
+  identifier                 = "qux"
+  location                   = "uksouth"
+  log_analytics_workspace_id = "quux"
+  resource_group_name        = "grault"
+  zone                       = "waldo"
+
+  tags = {
+    Foo = "Bar"
+  }
+}
