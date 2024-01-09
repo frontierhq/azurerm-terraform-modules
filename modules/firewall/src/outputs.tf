22efff118b4efe28410fadf5a1cf0f3a05c6f9f3
Fraser Davidson <fraser@gofrontier.com>
2024-01-09 18:09:33 +0000

    Add Firewall module

---
+++ modules/firewall/src/outputs.tf
@@ -0,0 +1,7 @@
+output "id" {
+  value = azurerm_firewall.main.id
+}
+
+output "name" {
+  value = azurerm_firewall.main.name
+}
---
+++ modules/firewall/src/variables.tf
@@ -0,0 +1,73 @@
+variable "dns_servers" {
+  type    = list(string)
+  default = null
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "firewall_policy_id" {
+  type    = string
+  default = null
+}
+
+variable "identifier" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "log_analytics_workspace_id" {
+  type = string
+}
+
+# https://learn.microsoft.com/en-us/azure/azure-monitor/reference/supported-logs/microsoft-network-azurefirewalls-logs
+variable "log_categories" {
+  type    = list(string)
+  default = []
+}
+
+variable "log_category_groups" {
+  type = list(string)
+  default = [
+    "allLogs"
+  ]
+}
+
+variable "metric_categories" {
+  type = list(string)
+  default = [
+    "AllMetrics"
+  ]
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "sku_name" {
+  type    = string
+  default = "AZFW_VNet"
+}
+
+variable "sku_tier" {
+  type    = string
+  default = "Standard"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "virtual_hub_id" {
+  type    = string
+  default = null
+}
+
+variable "zone" {
+  type = string
+}
