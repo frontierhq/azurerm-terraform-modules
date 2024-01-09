22d5796029df7c7cea233dd9b999ba3d1d2293e7
Fraser Davidson <fraser@gofrontier.com>
2024-01-09 16:33:26 +0000

    WIP

--- modules/point-to-site-vpn-gateway/src/main.tf
+++ modules/point-to-site-vpn-gateway/src/main.tf
@@ -18,3 +18,33 @@ resource "azurerm_point_to_site_vpn_gateway" "main" {
 
   tags = merge(var.tags, local.tags)
 }
+
+resource "azurerm_monitor_diagnostic_setting" "main" {
+  name                       = "log-analytics"
+  target_resource_id         = azurerm_key_vault.main.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  dynamic "enabled_log" {
+    for_each = var.log_categories
+
+    content {
+      category = enabled_log.value
+    }
+  }
+
+  dynamic "enabled_log" {
+    for_each = var.log_category_groups
+
+    content {
+      category_group = enabled_log.value
+    }
+  }
+
+  dynamic "metric" {
+    for_each = var.metric_categories
+
+    content {
+      category = metric.value
+    }
+  }
+}
