a0732b9bce60e122a685fabe743008fe92b93bdd
Darrell Bolger <darrell.bolger@prudential.co.uk>
2024-01-24 09:24:34 +0000

    Cognitive outputs

--- modules/cognitive-account/src/outputs.tf
+++ modules/cognitive-account/src/outputs.tf
@@ -2,6 +2,18 @@ output "id" {
   value = azurerm_cognitive_account.main.id
 }
 
+output "endpoint" {
+  value = azurerm_cognitive_account.main.endpoint
+}
+
 output "name" {
   value = azurerm_cognitive_account.main.name
+}
+
+output "primary_access_key" {
+  value = azurerm_cognitive_account.main.primary_access_key
+}
+
+output "secondary_access_key" {
+  value = azurerm_cognitive_account.main.secondary_access_key
 }
