050a62dd50d5b578d7ff25a3083913828751aac6
Fraser Davidson <fraser@frontierhq.com>
2024-05-31 09:18:16 +0100

    Update Storage Account module to output access keys (#69)

--- modules/storage-account/src/outputs.tf
+++ modules/storage-account/src/outputs.tf
@@ -5,3 +5,11 @@ output "id" {
 output "name" {
   value = azurerm_storage_account.main.name
 }
+
+output "primary_access_key" {
+  value = azurerm_storage_account.main.primary_access_key
+}
+
+output "secondary_access_key" {
+  value = azurerm_storage_account.main.secondary_access_key
+}
