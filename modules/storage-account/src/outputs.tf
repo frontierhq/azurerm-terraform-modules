c5b6754f069496609b5e3f53f1987bd63a7a327b
Fraser Davidson <fraser@frontierhq.com>
2024-05-31 11:11:14 +0100

    WIP

--- modules/storage-account/src/outputs.tf
+++ modules/storage-account/src/outputs.tf
@@ -10,6 +10,10 @@ output "primary_access_key" {
   value = azurerm_storage_account.main.primary_access_key
 }
 
+output "primary_web_internet_endpoint" {
+  value = azurerm_storage_account.main.primary_web_internet_endpoint
+}
+
 output "secondary_access_key" {
   value = azurerm_storage_account.main.secondary_access_key
 }
