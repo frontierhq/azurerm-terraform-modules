4168aad3b0968c22c2023f24343f54d4a9ee2245
Fraser Davidson <fraser@frontierhq.com>
2024-06-19 23:38:59 +0100

    WIP

--- scripts/test.py
+++ scripts/test.py
@@ -1,13 +1,8 @@
 import os
 from helpers.test_terraform import test_terraform
-from tempfile import TemporaryDirectory
 
 
 if __name__ == "__main__":
-    temp_dir = TemporaryDirectory()
-
     for dir in [f.path for f in os.scandir(os.path.join(os.getcwd(), "modules")) if f.is_dir()]:
         test_terraform(os.path.join(dir, "src"), False)
         test_terraform(os.path.join(dir, "test"))
-
-    temp_dir.cleanup()
