283b39c573b69bf435d87152cd21bb809ea5c8a3
Fraser Davidson <fraser@frontierhq.com>
2024-06-19 22:50:10 +0100

    WIP

--- scripts/test.py
+++ scripts/test.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     env = {"TF_PLUGIN_CACHE_DIR": temp_dir.name}
     for dir in [f.path for f in os.scandir(os.path.join(os.getcwd(), "modules")) if f.is_dir()]:
-        test_terraform(os.path.join(dir, "src"), False, env=env)
-        test_terraform(os.path.join(dir, "test"), env=env)
+        test_terraform(os.path.join(dir, "src"), False, env=env, lockfile="readonly")
+        test_terraform(os.path.join(dir, "test"), env=env, lockfile="readonly")
 
     temp_dir.cleanup()
