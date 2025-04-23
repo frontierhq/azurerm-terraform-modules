b7886e54179a031e92d244ee6d903ea4fda6c68a
Fraser Davidson <fraser@frontierhq.com>
2025-04-23 21:48:38 +0100

    WIP

--- modules/virtual-network-gateway/src/data.tf
+++ modules/virtual-network-gateway/src/data.tf
@@ -1,6 +1,1 @@
 data "azurerm_client_config" "main" {}
-
-# See https://learn.microsoft.com/en-us/azure/vpn-gateway/openvpn-azure-ad-tenant#authorize-the-azure-vpn-application
-data "azuread_service_principal" "azure_vpn" {
-  display_name = "Azure VPN"
-}
