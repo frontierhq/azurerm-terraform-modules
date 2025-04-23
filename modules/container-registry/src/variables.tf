b2a85d40743ee9919e1efb1721c4019328e16b77
Fraser Davidson <fraser@frontierhq.com>
2025-04-23 08:37:31 +0100

    WIP

---
+++ modules/container-registry/src/variables.tf
@@ -0,0 +1,73 @@
+variable "admin_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "identifier" {
+  type = string
+}
+
+variable "identity" {
+  type = object({
+    type         = string
+    identity_ids = optional(list(string))
+  })
+  default = {
+    type         = "SystemAssigned"
+    identity_ids = []
+  }
+}
+
+variable "location" {
+  type = string
+}
+
+variable "log_analytics_workspace_id" {
+  type = string
+}
+
+# https://learn.microsoft.com/en-us/azure/azure-monitor/reference/supported-logs/microsoft-containerregistry-registries-logs
+variable "log_categories" {
+  type    = list(string)
+  default = []
+}
+
+variable "log_category_groups" {
+  type    = list(string)
+  default = []
+}
+
+# https://learn.microsoft.com/en-us/azure/azure-monitor/reference/supported-metrics/microsoft-containerregistry-registries-metrics
+variable "metric_categories" {
+  type = list(string)
+  default = [
+    "AllMetrics",
+  ]
+}
+
+variable "public_network_access_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "sku" {
+  type    = string
+  default = "Standard"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "zone" {
+  type = string
+}
