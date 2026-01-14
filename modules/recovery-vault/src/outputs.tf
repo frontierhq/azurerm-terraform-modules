9b79b7b344d5562c81d5b37dde99eac25f2ecaf3
Charlotte Anderson <charlotte.anderson@frontierhq.com>
2026-01-14 14:53:15 +0700

    RSV outputs

--- modules/recovery-vault/src/outputs.tf
+++ modules/recovery-vault/src/outputs.tf
@@ -1,7 +1,7 @@
 output "id" {
-  value = azurerm_resource_group.main.id
+  value = azurerm_recovery_services_vault.main.id
 }
 
 output "name" {
-  value = azurerm_resource_group.main.name
+  value = azurerm_recovery_services_vault.main.name
 }
