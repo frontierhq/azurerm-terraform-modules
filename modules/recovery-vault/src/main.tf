2f0c9d81ba91e854a30571deadcd8412592456af
Charlotte Anderson <charlotte.anderson@frontierhq.com>
2026-01-14 14:49:51 +0700

    TF fmt

--- modules/recovery-vault/src/main.tf
+++ modules/recovery-vault/src/main.tf
@@ -10,8 +10,8 @@ resource "azurerm_recovery_services_vault" "main" {
   storage_mode_type   = var.storage_mode_type
   soft_delete_enabled = var.soft_delete_enabled
 
-  cross_region_restore_enabled   = var.cross_region_restore_enabled
-  public_network_access_enabled  = var.public_network_access_enabled
+  cross_region_restore_enabled  = var.cross_region_restore_enabled
+  public_network_access_enabled = var.public_network_access_enabled
 
   identity {
     type         = "SystemAssigned, UserAssigned"
